942dbb7e99666f52daaed77559a21dd6567dd836
akash <akasheldo@hotmail.com>
2021-03-26 13:03:35 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var java = require("java");
+var mvn = require("node-java-maven");
+
+const className = "org.openkoreantext.processor.OpenKoreanTextProcessorJava";
+
+mvn(function (err, mvnResults) {
+  if (err) {
+    return console.error("could not resolve maven dependencies", err);
+  }
+  mvnResults.classpath.forEach(function (c) {
+    console.log("adding " + c + " to classpath");
+    java.classpath.push(c);
+  });
+
+  var result = java.callStaticMethodSync(
+    className,
+    "normalize",
+    "한국어를 처리하는 예시입니닼ㅋㅋㅋㅋㅋ #한국어"
+  );
+
+  console.log(result);
+});
